4305e04ddc23bd07bc8e7d1b9d758c642b3a5530
John Pilar <pilarjan4111@gmail.com>
2019-10-12 13:42:05 +0200

    navbar + refactoring

--- src/index.php
+++ src/index.php
@@ -1,8 +1,7 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "vendor". DIRECTORY_SEPARATOR. "autoload.php";
+require_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
-use PilaLib\Database;
 use PilaLib\Utilities;
 use PilaLib\Auth;
 
@@ -11,21 +10,26 @@ $auth = new Auth();
 
 session_start();
 
+// Login check
 $auth->isLogged($_SESSION['logged']);
-$db = Database::getInstance()->getConnection();
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Homepage</title>
+    <link rel="stylesheet" type="text/css" href="../dist/bundle.css">
 </head>
-<body>
-    <a href="logout.php">Odhlásit se</a>
-</body>
-</html>
 
+<body>
+    <?php include('partials/navbar.php') ?>
+
+    <script src="../dist/bundle.js"></script>
+</body>
+
+</html>
---
+++ src/partials/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="../dist/img/truck.png" alt="" height="50px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="logout.php">Odhlásit se</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
